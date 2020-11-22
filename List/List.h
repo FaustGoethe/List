112bcd83680bae2d387547f4fd76701b8b858f25
kunansy <kolobov.kirill@list.ru>
2020-11-22 22:47:48 +0300

    Change commit
    Header file was changed

--- List/List.h
+++ List/List.h
@@ -7,89 +7,82 @@
 #include <fstream>
 #include <string>
 
-using std::string;
 
 namespace SLL{
-	class List
-	{	
+    struct content{
+        int value;
+        size_t index;
+
+		friend std::ostream& operator<<(std::ostream&, const content&) noexcept;
+		content& operator=(int);
+    }
+    
+
+    struct list{
+        content value;
+        list* next; // the pointer links to the next element or NULL
+
+		friend std::ostream& operator<<(std::ostream&, const list&) noexcept;
+    }
+
+
+	class List{	
 	public:
-		struct content{
-			int key;
-			size_t index;
-
-			friend std::ostream& operator<<(std::ostream&, const content&) noexcept;
-
-			content& operator=(int);
-		};
-	    struct list{
-			content value;
-			list* next;
-
-			friend std::ostream& operator<<(std::ostream&, const list&) noexcept;
-		};
-		List()					noexcept;
-		List(const List&)		noexcept;
-		List(const int*, int)	noexcept;
-		List(size_t, int)		noexcept;
+		List()					noexcept; // base constructor
+		List(const List&)		noexcept; // copying constructor
+		List(const int*, int)	noexcept; // from array
+		List(size_t, int)		noexcept; // some values
 		~List()					noexcept;
 
 		List& operator()(const List&) noexcept;
 		List& operator= (const List&) noexcept;
 
-		void push	(int) noexcept;
-		void remove	(int);
+		void push(int) noexcept;
+		void remove(int); // remove by value
+		void pop(size_t); // remove by index
+
+		void push_back(int)  noexcept;
+		void push_front(int) noexcept;
 		
-		void remove_at_index(size_t);
+		void pop_back()  noexcept;
+		void pop_front() noexcept;
 
-		void push_back	(int) noexcept;
-		void push_front	(int) noexcept;
-		
-		void pop_back  () noexcept;
-		void pop_front () noexcept;
+		size_t size()  const noexcept;
+		bool   empty() const noexcept;
 
-		size_t	size()	const noexcept;
-		bool	empty()	const noexcept;
-
-		double averege()	const noexcept;
-		int max()			const noexcept;
-		int min()			const noexcept;
+		double averege() const noexcept;
+		int max()		 const noexcept;
+		int min()		 const noexcept;
 
 		int count(int) const;
 
 		list* _begin() const noexcept;
 		list* _end()   const noexcept;
 
-		void ksort(); // По ключам
-		void isort(); // По ключам в обратном порядке
-
-		void rksort(); // По индексам
-		void irsort(); // По индексам в обратном порядке
+		void vsort(bool); // sort by values, key of reverse sort
+		void isort(bool); // sort by indexes, key of reverse sort
 
 		friend std::ostream& operator<<(std::ostream&, const List&) noexcept;
-		friend std::istream& operator>>(std::istream&,		 List&) noexcept;
-
-		friend std::ofstream& operator<<(std::ofstream&, const  List&);
-		friend std::ifstream& operator>>(std::ifstream&,		List&);
+		friend std::ofstream& operator<<(std::ofstream&, const List&);
 		
 		content& operator[](size_t);
 		content& operator[](size_t) const;
 
-		string getHash(size_t) const;
+        std::string getHash(size_t) const;
 
 		void clear() noexcept;
 
-		void resize(size_t)			noexcept;
 		void resize(size_t, int)	noexcept;
+		void resize(size_t)			noexcept;
 
 		void reverse();
-
 	private:
-		list* begin; 
+		list* begin;
 
 		void Indexation() noexcept;
 		
 		int	recevingExistCodes(int)	const noexcept;
-		size_t getControlSum(const string&) const noexcept;
+		size_t getControlSum(const std::string&) const noexcept;
 
 		list* _end() noexcept;
 		//get type
